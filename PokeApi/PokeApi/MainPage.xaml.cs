02d6ed970c71d6ef8d2684666488f003b8a5230d
Louis Monié <louis.monie@etu.unilim.fr>
2022-02-07 08:07:47 +0100

    Ajoutez des fichiers projet.

---
+++ PokeApi/PokeApi/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PokeApi
+{
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
